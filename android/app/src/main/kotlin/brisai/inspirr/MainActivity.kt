49cb0479a73817fa89cc00a4fdd2f314e5d731de
Alejandro Navarrete <anavarrete.ga@gmail.com>
2020-02-07 10:42:32 -0600

    Initial sample Flutter app

---
+++ android/app/src/main/kotlin/brisai/inspirr/MainActivity.kt
@@ -0,0 +1,12 @@
+package brisai.inspirr
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
